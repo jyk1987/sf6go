c39e0a19717ce2cfdfaa6a9b134eeee9bdfb3058
贾育锟 <jiayukun@kuaibang360.com>
2022-02-07 19:00:58 +0800

    fix include

--- FaceLandmarker.go
+++ FaceLandmarker.go
@@ -3,5 +3,5 @@ package sf6go
 // #cgo CXXFLAGS: -std=c++1z -Wall -O3 -DNDEBUG -march=native
 // #cgo LDFLAGS: -L./lib -lSeetaFaceLandmarker600
 // #include <stdlib.h>
-// #include "FaceDetector_warp.h"
+// #include "FaceLandmarker_warp.h"
 import "C"
